9df92aeb42221e595f55e633030941c20fd200b1
jg2503 <gravatt.jay@gmail.com>
2016-12-20 23:25:18 +0000

    Import to dataframe, not json file dump

--- import.py
+++ import.py
@@ -1,15 +1,24 @@
 import requests
 import json
+import pandas as pd
+import numpy
 
 FPL_DATA_PATH = 'https://fantasy.premierleague.com/drf/'
 
 fpl_data = requests.get(FPL_DATA_PATH+'bootstrap-static').json()
-print 'request1 sorted'
+print('request1 sorted')
 for i, player in enumerate(fpl_data['elements']):
-    print player['id']
+    print(player['id'])
     fpl_data['elements'][i]['history'] = requests.get(FPL_DATA_PATH+'element-summary/'+str(player['id'])).json()
 
-print 'opening'
-f = open('data20161205.json', 'w')
-f.write(json.dumps(fpl_data, sort_keys=True, indent=4, separators=(',', ': ')))
-f.close()
+frame = pd.DataFrame()
+for i, player in enumerate(fpl_data['elements']):
+	q = pd.DataFrame(fpl_data['elements'][i]['history']['history'])
+	q['first_name'] = fpl_data['elements'][i]['first_name']
+	q['second_name'] = fpl_data['elements'][i]['second_name']
+	q['game_week'] = 'GW' + q['round'].map(str)
+	frame = pd.concat([frame,q])
+	frame = frame.set_index(['game_week','first_name','second_name'])
+
+print('opening')
+
